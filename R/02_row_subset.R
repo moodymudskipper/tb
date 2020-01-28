2d00ebe09f88aa4b9412ec1b6257c7b80eb41f4a
moodymudskipper <antoine.fabri@gmail.com>
2020-01-28 16:41:04 +0100

    removed obsolete comments

--- R/02_row_subset.R
+++ R/02_row_subset.R
@@ -1,5 +1,6 @@
 # evaluate .i in the context of the data frame, handle .by if given
-row_subset_by_ref <- function(.i, mask){ #env = parent.frame()){
+row_subset_by_ref <- function(.i, mask){
+
   ## evaluate in  mask
   .X <- mask$.X
   .i <- eval(.i, envir=.X, enclos= mask)
@@ -49,47 +50,3 @@ row_subset_by_ref <- function(.i, mask){ #env = parent.frame()){
   }
   invisible()
 }
-
-
-#
-#
-# # evaluate .i in the context of the data frame, handle .by if given
-# simplify_i <- function(.X, .i, env = parent.frame()){
-#   ## evaluate it in the context of data frame and mask
-#   .i <- eval(.i, envir=c(as.list(.X), list(.X = .X)), enclos= env)
-#   if(inherits(.i, "formula")) {
-#     one_sided_formula_lgl <- length(.i) == 2
-#     if(one_sided_formula_lgl) {
-#       ## use regex on rows
-#       .i <- grepl(eval(.i[[2]], env), rownames(.X))
-#     } else {
-#       ## extract variables from rhs
-#       along_vars <- get_all_vars(.i[-2], .X)
-#       ## evaluate lhs
-#       .i <- eval(.i[[2]], envir=.X, enclos= env)
-#       if(!is.numeric(.i)) {
-#         stop("if using `.by` and `.i` non missing, `.i` must be numeric")
-#       }
-#       ## build ave call to get indices by group
-#       call <- as.call(c(
-#         quote(ave),                         # ave(
-#         list(x = seq(nrow(.X))),            #   seq(nrow(.X)),
-#         along_vars,                         #   grpvar1, grpvar2, ...,
-#         list(FUN = seq_along)))             #   Fun = seq_along)
-#       ## evaluate it and filter these indices on given values of .i
-#       .i <- eval(call, envir = .X) %in% .i
-#     }
-#   } else {
-#     if(is.logical(.i)) {
-#       ## turn NAs to FALSE so we keep only TRUE indices
-#       .i[is.na(.i)] <- FALSE
-#     } else if (is.data.frame(.i)) {
-#       ## get the indices by doing a semi join
-#       nms <- intersect(names(.X), names(.i))
-#       .i <- .i[,nms]
-#       .i$`*temp*` <- 1
-#       .i <- !is.na(merge(.X[nms], .i, all.X = TRUE)$`*temp*`)
-#     }
-#   }
-#   .i
-# }

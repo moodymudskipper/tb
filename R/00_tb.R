b0ad57c6cef83389276a6aeb53f71f6a515a2571
moodymudskipper <antoine.fabri@gmail.com>
2020-01-28 16:00:44 +0100

    improved tb and print.tb

--- R/00_tb.R
+++ R/00_tb.R
@@ -1,18 +1,48 @@
 
 #' Create tb object
 #'
-#' `tb()` works a lot like `data.frame()`
+#' `tb()` works a lot like `data.frame()`, but allows for `list` or `data.frame`
+#' columns, and outputs a data frame with the additional class `tb`
 #' @export
 tb <- function(...){
-  # we don't wrap data.frame because it's not nice with list or data frame columns
+  dots <- eval(substitute(alist(...)))
+  # dots <- lapply(dots, function(x) {
+  #   if(is.numeric(x) || is.character(x)) return(list(x))
+  #   if(has_splice_prefix(x)){
+  #     return(eval.parent(x[[2]]))
+  #   }
+  #   x
+  # })
+  # dots <- unlist(dots, recursive = FALSE))
+
+  ## deal with names
+  dots_chr <- as.character(dots)
   x <- list(...)
-  structure(x, class=c("tb","data.frame"), row.names = c(NA, - max(lengths(x))))
+  empty_nms_lgl <- allNames(x) == ""
+  names(x)[empty_nms_lgl] <- dots_chr[empty_nms_lgl]
+  ## recycle or fail
+  lengths_ <- vapply(x, NROW, integer(1))
+  n <- max(lengths_)
+  if (!all(lengths_ %in% c(1, n)))
+    stop("columns must have consistent lengths, only values of length one are recycled")
+  for(i in which(lengths_ != n)){
+    if (is.data.frame(x[[i]])) {
+      x[[i]] <- do.call("rbind",replicate(n, x[[i]], FALSE))
+    } else if (is.list(x)){
+      x[[i]] <- do.call("c",replicate(n, x[[i]], FALSE))
+    } else {
+      x[[i]] <-rep(x[[i]],n)
+    }
+  }
+  ## create object
+  structure(x, class=c("tb","data.frame"), row.names = c(NA, - n))
 }
 
 #' @export
 as_tb <- function(x){
-  x <- as.data.frame(x)
-  class(x) <- c("tb", class(x))
+  if(!is.data.frame(x))
+    stop("`x` must be a data frame, apply as.data.frame on the object first if necessary")
+  class(x) <- c("tb", "data.frame")
   x
 }
 
@@ -60,13 +90,25 @@ backup_class <- function(x){
   class_
 }
 
-#withDT:::restore_attr
-
 # it will do for now!
-print.tb <- function(x) {
+print.tb <- function(x, n = 10) {
   cat("# A tb:", nrow(x), "x", ncol(x), "\n")
-  n <- nrow(x)
-  print.data.frame(head(x,10))
-  if(n > 10) cat("# ... with ", n-10," more rows")
+  nr <- nrow(x)
+  n_min <- min(nr, n)
+  converter <- function(x) {
+    sapply(x, function(elt){
+    if(is.data.frame(elt)) {
+      sprintf("[%s x %s]", nrow(elt), ncol(elt))
+    } else elt
+    })
+  }
+
+  x_edited <- x
+  is_list_lgl <- sapply(x, is.list)
+  x_edited[is_list_lgl] <- lapply(x[is_list_lgl], converter)
+  #x_edited <- x[is.list(~~.data) := converter(~~.)]
+
+  print.data.frame(x_edited[seq_len(n_min),])
+  if(nr > n_min) cat("# ... with ", nr-n_min, " more rows")
   invisible(x)
 }
